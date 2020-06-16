7e434fcab69c0a1e4f1e502047e17acc228416e9
lijiayin0420 <934432174@qq.com>
2020-06-16 17:24:59 +0800

    完善登录之后的操作

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -62,8 +62,14 @@ export default {
       this.$refs.loginFormRef.validate(async (valid) => {
         if (!valid) return
         const { data: res } = await this.$http.post('login', this.loginForm)
-        if (res.meta.status !== 200) return console.log('登录失败')
-        console.log('登录成功')
+        if (res.meta.status !== 200) return this.$message.error('登录失败！')
+        this.$message.success('登录成功！')
+        // 1. 将登录成功之后返回的token，保存在客户端的sessionStorage中
+        //  1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //  1.2 token 只应在当前网站打开期间生效，所以token保存在sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
       })
     }
   }
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
@@ -13,6 +14,10 @@ const router = new Router({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
