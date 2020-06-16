8d4f468ef6a0062d2ab7b5b2ab4feebb8360a111
lijiayin0420 <934432174@qq.com>
2020-06-16 15:31:39 +0800

    添加登录页面的数据绑定

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,95 @@
 <template>
-  <div>
-    登录组件
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登录表单区 -->
+      <el-form :model="loginForm" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input
+            v-model="loginForm.useername"
+            prefix-icon="el-icon-s-custom"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="el-icon-lock"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        useername: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: solid 1px #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  box-sizing: border-box;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
