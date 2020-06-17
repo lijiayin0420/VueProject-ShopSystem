c21d5be4843cd99dba0fa239d7388ddde524c8af
lijiayin0420 <934432174@qq.com>
2020-06-17 11:07:04 +0800

    通过axios请求拦截器添加token

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+// 添加请求拦截器
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
