f94eb8c88bc1e40c8576aaf6bbe2a6b306e6c0c8
lijiayin0420 <934432174@qq.com>
2020-06-18 16:26:37 +0800

    实现 删除权限 操作

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -28,7 +28,9 @@
             >
               <!-- 渲染一级权限 -->
               <el-col :span="5">
-                <el-tag>{{ item1.authName }}</el-tag>
+                <el-tag closable @close="removeRightById(scope.row, item1.id)">
+                  {{ item1.authName }}
+                </el-tag>
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <!-- 渲染二级和三级权限 -->
@@ -40,7 +42,13 @@
                   :key="item2.id"
                 >
                   <el-col :span="6">
-                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <el-tag
+                      type="success"
+                      closable
+                      @close="removeRightById(scope.row, item2.id)"
+                    >
+                      {{ item2.authName }}
+                    </el-tag>
                     <i class="el-icon-caret-right"></i>
                   </el-col>
                   <el-col :span="18">
@@ -48,6 +56,8 @@
                       type="warning"
                       v-for="item3 in item2.children"
                       :key="item3.id"
+                      closable
+                      @close="removeRightById(scope.row, item3.id)"
                     >
                       {{ item3.authName }}
                     </el-tag>
@@ -100,6 +110,33 @@ export default {
       }
 
       this.rolesList = res.data
+    },
+    // 根据 id 删除对应的权限
+    async removeRightById(role, rightid) {
+      // 弹框提示是否确认删除
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该用户, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消删除')
+      }
+
+      const { data: res } = await this.$http.delete(
+        `roles/${role.id}/rights/${rightid}`
+      )
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败')
+      }
+
+      role.children = res.data
     }
   }
 }
