9023724e252aaef9c2ff50dff9cedd9a8ffd4347
lijiayin0420 <934432174@qq.com>
2020-06-19 14:13:06 +0800

    使用tree-table渲染商品分类表格

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -16,6 +16,25 @@
       </el-row>
 
       <!-- 表格 -->
+      <tree-table
+        :data="catelist"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        show-index
+        index-text="#"
+        border
+        :show-row-hover="false"
+      >
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted === false"
+            style="color: lightgreen"
+          ></i>
+          <i class="el-icon-error" v-else style="color: red"></i>
+        </template>
+      </tree-table>
 
       <!-- 分页区域 -->
     </el-card>
@@ -25,7 +44,48 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表，默认为空
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 表示将当前列定义为模版列
+          type: 'template',
+          // 表示当前列使用模版名称
+          template: 'isok'
+        }
+      ]
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    // 获取商品分类数据
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.queryInfo
+      })
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败')
+      }
+
+      this.catelist = res.data.result
+      this.total = res.data.total
+    }
   }
 }
 </script>
