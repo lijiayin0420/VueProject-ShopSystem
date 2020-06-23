2bc811045fa0dd483996816c2793c2a24c13ce3d
lijiayin0420 <934432174@qq.com>
2020-06-23 11:27:43 +0800

    渲染商品列表数据，自定义全局格式化时间过滤器

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
+import List from './components/goods/List.vue'
 
 Vue.use(Router)
 
@@ -49,6 +50,10 @@ const router = new Router({
         {
           path: '/params',
           component: Params
+        },
+        {
+          path: '/goods',
+          component: List
         }
       ]
     }
