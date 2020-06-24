ad89a7f0504c5edcc4ed93fd754173983ac6018e
lijiayin0420 <934432174@qq.com>
2020-06-24 11:47:32 +0800

    渲染 数据报表

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -10,7 +10,7 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <!-- 2.为Echarts准备一个具备大小（宽高）的Dom -->
-      <div id="main" style="width: 600px;height:400px;"></div>
+      <div id="main" style="width: 750px;height:400px;"></div>
     </el-card>
   </div>
 </template>
@@ -18,41 +18,51 @@
 <script>
 // 1.导入 echarts
 import echarts from 'echarts'
+import _ from 'lodash'
 
 export default {
   data() {
-    return {}
+    return {
+      // 需要合并的数据
+      option: {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    }
   },
   created() {},
   // 此时页面上的元素已经被渲染完毕
-  mounted() {
+  async mounted() {
     // 3.基于准备好的Dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'))
 
-    // 4.准备数据和配置项
-    var option = {
-      title: {
-        text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量']
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [
-        {
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
+    const { data: res } = await this.$http.get('reports/type/1')
+
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败')
     }
 
+    // 4.准备数据和配置项
+    const result = _.merge(res.data, this.option)
+
     // 5.展示数据
-    myChart.setOption(option)
+    myChart.setOption(result)
   },
   methods: {}
 }
